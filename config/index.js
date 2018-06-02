bc06ce089709175961ea72038f95c29c962619c4
viv <ymviv@qq.com>
2018-06-02 23:51:18 +0800

    fix config

---
+++ config/index.js
@@ -0,0 +1,16 @@
+const config = {
+  port: '7304',
+  
+  mongodb: {
+    url: 'mongodb://localhost:27017/hackathon',
+    username_index: 'username_1',
+  },
+
+  mail: {
+  }
+};
+
+module.exports = {
+  ...config,
+  ...(process.env.NODE_ENV == 'production' ? require('./production') : require('./development')),
+}
