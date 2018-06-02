ae0a1e538799136990acc59c4967d11c7cd67fcc
viv <ymviv@qq.com>
2018-06-03 01:58:05 +0800

    feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -8,7 +8,7 @@ module.exports = {
       
       const newsSet = []
       for(let source of JSON.parse(sources)) {
-        const news = await FeedModel.find({ _id: ctx.session.passport.user._id })
+        const news = await UserFeedModel.find({ user: ctx.session.passport.user._id, source })
                                     .skip(delta || 0).limit(limit).sort({ time: -1 });
         if(news) {
           newsSet.splice(-1, 0, ...news);
