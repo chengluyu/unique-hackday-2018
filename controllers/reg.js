89a595f386a41093fa0efbd59b1e912e2aeae70c
viv <ymviv@qq.com>
2018-06-03 11:11:47 +0800

    fix reg

--- controllers/reg.js
+++ controllers/reg.js
@@ -1,7 +1,32 @@
+const axios = require('axios');
+const recommend = require('../libs/recommend');
 const UserModel = require('../models/user');
-//const mq = require('../libs/mq');
 const config = require('../config').mongodb;
 const schemas = require('../schemas');
+
+async function fetchInterestTopics(shortname) {
+  const fetchURL = `https://www.zhihu.com/api/v4/members/${shortname}/following-topic-contributions?include=data[*].topic.introduction`;
+  try {
+    const topics = [];
+    while (true) {
+      const { data } = await axios({
+        method: 'GET',
+        headers: {
+          "Authorization": "oauth c3cef7c66a1843f8b3a9e6a1e3160e20"
+        },
+        url: fetchURL + `&offset=${topics.length}&limit=20`
+      });
+      topics.push(...data.data.map(x => x.topic));
+      if (data.paging.is_end) {
+        break;
+      }
+    }
+    return topics;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 module.exports = async (ctx, next) => {
   console.log(ctx.request.body);
@@ -12,6 +37,10 @@ module.exports = async (ctx, next) => {
     console.log(ctx.request.body);
     try {
       const res = await user.save();
+      const topics = await fetchInterestTopics(username);
+      for(const topic of topics || []) {
+        await recommend.liked(username, topic.id);
+      }
     } catch(e) {
       const { code, errmsg } = e.toJSON();
       if(code === 11000) {
