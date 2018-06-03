aa4b9146d10da3b193541629faffaef6ffbcc5f7
viv <ymviv@qq.com>
2018-06-03 11:44:05 +0800

    fix rec

--- libs/recommend.js
+++ libs/recommend.js
@@ -1,8 +1,11 @@
 const raccoon = require('raccoon');
 
-const zhihu = require('../data/zhihu.json').slice(100);
+const zhihu = require('../data/zhihu.json').slice(50);
 zhihu.map((user) => {
   const username = user.user.fullname;
+  if(user.topics && user.topics.length > 20) {
+    user.topics = user.topics.slice(20);
+  }
   for(const topic of user.topics || []) {
     raccoon.liked(username, topic.id);
   }
