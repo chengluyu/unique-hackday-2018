9165acf0391a546168297fbb2499812b5d4600f6
viv <ymviv@qq.com>
2018-06-03 12:59:45 +0800

    fix rec

--- tools/converter.douban.js
+++ tools/converter.douban.js
@@ -2,3 +2,5 @@ const fs = require('fs');
 
 const likes = require('../data/rsp_decode_597c405f65b391dc.json');
 const users = require('../data/rsp_decode_9ba9d2a3d53a01f4.json');
+
+console.log(likes.interests);
